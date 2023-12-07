67d1a6d64a502a9cd9d97ad31a463d200454da9c
NoaVHD <noavanhoorde@gmail.com>
2023-12-07 08:24:14 +0100

    z

--- src/main/java/fr/dampierre/App.java
+++ src/main/java/fr/dampierre/App.java
@@ -11,21 +11,46 @@ public class App {
         int nbLancer = 5;
         int emplacement = 0;
         int resteCase = 20;
+        int i = 1;
+        int lancer=0;
 
+        lancer(nbLancer, emplacement, resteCase, scanner);
+        resultat(emplacement, place);
+
+       // for(int i= 1; i <= nbLancer; i++){
+        //    scanner.nextLine();
+        //   Random generateur = new Random();
+        //    int lancer = generateur.nextInt(6) + 1;
+        //    emplacement = emplacement + lancer;
+        //    resteCase = resteCase - lancer;
+
+           // System.out.println("Lancer" + i + " : vous avez fait " + lancer + " vous êtes sur la case "+ emplacement+ "(encore " + resteCase + " case)");
+//}
+   // if (emplacement == place ) {
+     //   System.out.println("Vous avez gagner !");
+    //}
+    //else{
+      //  System.out.println("Vous avez perdu !");
+    //}
+}
+    public static void lancer(int nbLancer, int emplacement, int resteCase, Scanner scanner) {
         for(int i= 1; i <= nbLancer; i++){
             scanner.nextLine();
             Random generateur = new Random();
             int lancer = generateur.nextInt(6) + 1;
             emplacement = emplacement + lancer;
             resteCase = resteCase - lancer;
-
             System.out.println("Lancer" + i + " : vous avez fait " + lancer + " vous êtes sur la case "+ emplacement+ "(encore " + resteCase + " case)");
-        }
+        
+    }
+}
+
+public static void resultat(int emplacement, int place){
     if (emplacement == place ) {
         System.out.println("Vous avez gagner !");
     }
     else{
         System.out.println("Vous avez perdu !");
     }
-}
+}    
 }
